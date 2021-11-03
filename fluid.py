21d231a1f1ae6f999ce4d451e6d624bfff389386
ArrogantGao <gaoxuanzhao@gmail.com>
2021-11-03 12:10:15 +0800

    more smooth

--- fluid.py
+++ fluid.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 from scipy import interpolate
+from scipy.misc import derivative
 import pylab as pl
 import matplotlib as mpl
 
@@ -13,11 +14,11 @@ def discrete_partial_d(A, x, y):
 
 
 def vector_field_gen(m, n):
-    m += 100
-    n += 100
-    m_ran = int(m/10)
-    n_ran = int(n/10)
-    x, y = np.mgrid[1:m+1:10, 1:n+1:10]
+    m += 1000
+    n += 1000
+    m_ran = int(m/100)
+    n_ran = int(n/100)
+    x, y = np.mgrid[1:m+1:100, 1:n+1:100]
     A_random = np.zeros((m_ran, n_ran)) #随机产生一个维度仅有100的源
     
     for a in range(int(m_ran)):
@@ -35,8 +36,8 @@ def vector_field_gen(m, n):
 
 
 def fluid_field_gen(A, m, n, v_max_given):
-    m += 100
-    n += 100
+    m += 1000
+    n += 1000
 
     v = np.zeros((m,n,2))
     for x in range(m):
@@ -46,10 +47,10 @@ def fluid_field_gen(A, m, n, v_max_given):
     for x in range(m):
         for y in range(n):
             v[x,y] = v[x,y] * v_max_given / v_max_local
-    v = v[50:m-50, 50:n-50, :]
+    v = v[500:m-500, 500:n-500, :]
     np.savetxt('v_x.txt', v[:,:,0], fmt='%f', delimiter=',')
     np.savetxt('v_y.txt', v[:,:,1], fmt='%f', delimiter=',')
     return v
     
 A = vector_field_gen(1000, 1000)
-v = fluid_field_gen(A, 1000, 1000, 9)
+v = fluid_field_gen(A, 1000, 1000, 2.5)
